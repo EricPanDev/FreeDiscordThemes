90476cf505c61c273f62c379fd431a1d0fcdb878
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2023-08-13 17:48:11 -0700

    Create Amoled.js

---
+++ Amoled.js
@@ -0,0 +1,5 @@
+// Developed by Eric Pan, MIT License. 
+
+// Nothing much to see here, amoled is already avaliable, just not in the appearance options
+
+document.body.classList.add("theme-amoled");
