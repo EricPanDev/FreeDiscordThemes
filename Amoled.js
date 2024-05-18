e4f93e20892d645fb1c0155f7d9960c7fc9617ed
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2024-05-18 02:42:40 -0700

    discord deleted amoled :(

--- Amoled.js
+++
@@ -1,5 +0,0 @@
-// Developed by Eric Pan, MIT License. 
-
-// Nothing much to see here, amoled is already avaliable, just not in the appearance options
-
-document.body.classList.add("theme-amoled");
